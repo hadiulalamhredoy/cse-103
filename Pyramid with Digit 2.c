0b028726aec305f14416d3130969c87a73e28f1c
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2024-07-01 22:53:52 +0600

    Add files via upload

---
+++ Pyramid with Digit 2.c
@@ -0,0 +1,32 @@
+/*              1
+               2 2
+              3 3 3
+*/
+#include <stdio.h>
+
+int main()
+{
+    int row , col , n , spc , k ;
+    printf("Enter your value for n please \n");
+    scanf("%d" , &n);
+
+    spc = (n + 4) - 1;
+    printf("Value of n is %d \n" , n);
+    printf("Your pattern looks like this\n");
+    for(row = 1 ; row <= n ; row++)
+    {
+        for(k = spc ; k >= 1 ; k--)
+        {
+             printf(" ");
+        }
+            for(col = 1 ; col <= row ; col++)
+            {
+                printf("%d " , row);
+            }
+        printf("\n");
+        spc--;
+    }
+    return 0;
+}
+    // This the END of this program.
+    // Thank You.
