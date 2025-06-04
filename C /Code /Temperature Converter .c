0477a8ecd78599298a9a6c8776ab65646717d5dc
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-04 15:53:24 +0600

    Update Temperature Converter .c

--- C /Code /Temperature Converter .c
+++ C /Code /Temperature Converter .c
@@ -8,3 +8,13 @@ scanf("%f",&c);
 F = (c* 1.8)+32;
 printf("Farn =.%2f",F);
 }
+//Convert from farenheit from Centegrade
+#include<stdio.h>
+int main()
+{
+float c,F;
+printf("Enter Centregrade=");
+scanf("%f",&c);
+F = (C * 1.8)/5
+printf("%f",A);
+}
