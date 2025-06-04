d50b0fcfaec98cba989a5fc0b77b3331ee490aea
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-05 00:53:06 +0600

    Create Parameter Triangle.c

---
+++ C /Code /Parameter Triangle.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+float a,b,c;
+printf("a = ");
+scanf("%f",&a);
+
+printf("b = ");
+scanf("%f",&b);
+
+printf("c = ");
+scanf("%f",&c);
+
+if ((a + b > c ) && (b + c > a) && (c + a > b))
+
+{
+float perimeter = a + b + c;
+printf("triangle is valid");
+printf("perimeter : %.2f\n",perimeter); }
+
+else{
+printf("The triangle is not valid\n");
+}
