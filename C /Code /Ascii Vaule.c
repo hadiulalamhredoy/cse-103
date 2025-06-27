c24cb926cc1348b4fd94576db71afa8408b81569
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-06-27 19:04:37 +0600

    Create Ascii Vaule.c

---
+++ C /Code /Ascii Vaule.c
@@ -0,0 +1,16 @@
+// Write a C programme to conmvert ascii vaule to ascii character
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+ 
+     int n;
+     printf("Enter an ASCII Vaule : ");
+     scanf("%d",&n);
+     
+     printf("The Ascii Character is : %c",n);
+ 
+
+    return 0;
+}
