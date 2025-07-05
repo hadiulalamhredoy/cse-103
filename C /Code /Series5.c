b26d03fc942cf859528722a330c9a9b07e25ee4d
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-07-05 21:35:04 +0600

    Create Series5.c

---
+++ C /Code /Series5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+double sum=0,n,i;
+printf("Enter n = ");
+scanf("%lf",&n);
+
+for(i=1;i<=n;i++)
+{
+sum = sum + (1/i);
+if(i==1)
+
+printf("\n1 + ");
+
+else if (i==n)
+printf("(1/%lf)",i);
+
+else
+printf("(1/%lf)+",i);
+
+}
+printf( " = %.2lf\n",sum);
+
+}
