11e36f578513c8e43752b4674f78a12b8f960a0e
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-09-21 00:43:28 +0600

    Update Structure.c

--- C /Code /Structure.c
+++ C /Code /Structure.c
@@ -113,6 +113,117 @@ struct person
 //concatenation without strcat()
 
 
+#include<stdio.h>
+struct person
+{
+    int age;
+    float salary;
+};
+    int main()
+    {
+
+       struct person person1 = {27,25500,25};
+       struct person person2,person3;
+
+       //elementwise assignment
+       person2.age =25;
+       person2.salary  = 277750.87;
+
+       person3 = person2;
+
+       if(person1.age==person2.age && person1.salary==person2.salary)
+         printf("Person 1 is equal to Person2");
+
+    else
+         printf("Person 1 is not equal to Person2");
+
+
+    }
+//Array of structure
+
+#include<stdio.h>  
+struct person
+{
+    int age;
+    float salary;
+};
+
+int main()
+{
+    struct Person person[4];
+    int i;
+    
+    for(i=0;i<4;i++)
+    {
+        printf("Enter information for  Person %d\n",i+1);
+        printf("Enter Age : ");
+        scanf("%d",&person[i].age);
+        
+        printf("Enter Salary : ");
+        scanf("%f",&person[i].salary);
+        
+        
+    }
+    
+    for(i=0;i<4;i++)
+    {
+        printf("Information for  Person %d\n",i+1);
+        printf("Age:%d",&person[i].age);
+        printf("Salary:%f",&person[i].salary);
+        
+        
+    }
+    
+    
+}
+
+// Array within structure
+
+#include<stdio.h>  
+struct person
+{
+    char name[50];
+    int age;
+    float salary;
+};
+
+int main()
+{
+    struct Person person[4];
+    int i;
+    
+    for(i=0;i<4;i++)
+    {
+        printf("Enter information for  Person %d\n",i+1);
+        print("Enter Name :");
+        gets(person[i].name);
+        printf("Enter Age : ");
+        scanf("%d",&person[i].age);
+        
+        printf("Enter Salary : ");
+        scanf("%f",&person[i].salary);
+        
+        
+    }
+    
+    for(i=0;i<4;i++)
+    {
+        printf("Information for  Person %d\n",i+1);
+        printf("Age:%d",&person[i].age);
+        
+        printf("Salary:%f",&person[i].salary);
+        
+        
+    }
+    
+    
+}
+
+// passing structure variable to function
+
+
+
+
 
 
 
