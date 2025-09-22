f7e6252c32ccd1b5eb758b01f59e48ea2c0c7593
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-09-22 12:30:45 +0600

    Update Structure.c

--- C /Code /Structure.c
+++ C /Code /Structure.c
@@ -264,6 +264,29 @@ int main()
 
 }
 
+//Introduction of a union  
+
+#include<stdio.h>
+union test{
+
+     int x,y;
+
+};
+
+
+int main()
+{
+    union test t1;
+    t1.x = 10;
+    printf("t1.x = %d\n",t1.x);
+    printf("t1.y =%d\n",t1.y);
+
+    t1.y = 10;
+    printf("t1.x = %d\n",t1.x);
+    printf("t1.y =%d\n",t1.y);
+
+}
+
 
 
 
