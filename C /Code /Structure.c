d8511348e866c684205c7c7c0451a32d6b2c84df
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-09-22 12:29:03 +0600

    Update Structure.c

--- C /Code /Structure.c
+++ C /Code /Structure.c
@@ -219,7 +219,50 @@ int main()
     
 }
 
-// passing structure variable to function
+//  size of union and structure
+#include<stdio.h>
+union test1
+{
+
+    int x,y;
+
+};
+
+union test2
+{
+    char ch;
+    int  x;
+};
+union test3
+{
+    char name[20];
+    double d;
+};
+
+struct test4
+{
+    int x;
+    int y;
+    char ch;
+
+};
+
+
+int main()
+{
+
+    union test1 t1;
+    union test2 t2;
+    union test3 t3;
+
+    struct test4 t4;
+
+    printf("sizeof(t1) = %d\n",sizeof(t1));
+    printf("sizeof(t2) = %d\n",sizeof(t2));
+    printf("sizeof(t3) = %d\n",sizeof(3));
+    printf("sizeof(t4)=  %d\n",sizeof(4));
+
+}
 
 
 
