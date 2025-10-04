463db0fadca77bc242446f924f24a1d1783db9a7
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-04 23:13:47 +0600

    Create DataType.c

---
+++ Code/DataType.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+        int a =  20;
+        char c  = 'a';
+        float f  =20.5;
+        double d = 4.5678;
+         bool b;
+          b  =  true ;
+        printf("%d ",a);
+        printf("%c",c);
+        printf("%f",f);
+        printf("%lf",lf);
+        printf("%d",b);
+
+}
