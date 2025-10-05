ef81b877b2a18bc0b4e0c78421f596aee2de364d
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-05 12:01:59 +0600

    Update DataType.c

--- Code/DataType.c
+++ Code/DataType.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
+#include<stdbool.h>
 int main()
 {
-
-        int a =  20;
-        char c  = 'a';
-        float f  =20.5;
-        double d = 4.5678;
-         bool b;
-          b  =  true ;
-        printf("%d ",a);
-        printf("%c",c);
-        printf("%f",f);
-        printf("%lf",lf);
-        printf("%d",b);
-
+    int a = 10;  
+    float f  = 12.5;  
+    char ch = 'A';  
+    double d =  12.4567;  
+    bool b  = true;  
+    
+    printf("%d\n",a);
+      printf("%f\n",f);
+        printf("%c\n",ch);
+          printf("%lf\n",d);
+            printf("%d",b);
+    
+    
+    
+    
 }
