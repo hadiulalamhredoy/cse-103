a90172fe59f2228a9ab43c329083586d0c14fea7
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-08 07:39:37 +0600

    Update Loop.c

--- Code/Loop.c
+++ Code/Loop.c
@@ -10,3 +10,16 @@ int main()
       
     
 }
+//Print the 100 to 1 Reverse Number Using For loop 
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=100;i<=1;i++)
+    {
+        printf("%d\n",i);
+    }
+    
+    
+    
+}
