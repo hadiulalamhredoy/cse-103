acf318cea66c1f1d86bfc5593094c83604708d3f
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-11 11:33:13 +0600

    Update Loop.c

--- Code/Loop.c
+++ Code/Loop.c
@@ -34,3 +34,23 @@ int main()
     }
     
 }
+
+//Print the sum to 1 from n  
+#include<stdio.h>
+int main()
+{
+
+       int n;  
+       scanf("%d",&n);  
+       int sum  = 0;  
+       for(int i=1;i<=n;i++)
+       {
+           sum  =  sum  + i;
+
+       }
+    
+         printf("%d\n",sum);  
+         
+         return 0;
+
+}
