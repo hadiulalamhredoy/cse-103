808bfa21e5c71fe34cdb87df14708d892a00aec8
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-16 19:46:24 +0600

    Create Array.c

---
+++ Code/Array.c
@@ -0,0 +1,15 @@
+//Declaration of a Array  
+#include<stdio.h>
+int main()
+{
+
+     int ar[5];  
+     ar[1] = 20;
+     ar[2] = 50;
+     ar[3] = 70;  
+     ar[4] = 60;  
+     ar[5] = 40;  
+     printf("%d",ar[5]);
+
+
+}
