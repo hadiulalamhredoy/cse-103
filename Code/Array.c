02a2fdae969e809fda8727dc05a9f851dddb12dc
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-16 19:51:33 +0600

    Update Array.c

--- Code/Array.c
+++ Code/Array.c
@@ -13,3 +13,21 @@ int main()
 
 
 }
+
+
+//Array initialization  
+#include<stdio.h>  
+int main()
+{
+
+   int x = 10;  
+   int ar[] = {10,20,30,40,50}; 
+   
+   for(int i= 0; i<5; i++)
+   {
+        printf("%d ",ar[i]);
+
+   }
+
+
+}
