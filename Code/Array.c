e6a2a390b5d55c2a656997a908ee27f93490a0fe
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-16 20:54:03 +0600

    Update Array.c

--- Code/Array.c
+++ Code/Array.c
@@ -31,3 +31,51 @@ int main()
 
 
 }
+
+
+//Sum of all values of an array
+#include<stdio.h>  
+int main()
+{
+     int n; 
+     scanf("%d",&n);  
+     int a[n]; 
+
+     for(int i = 0; i<n; i++)
+     {
+        scanf("%d",&a[i]); 
+     }
+     int sum  = 0;  
+
+     for(int i =0; i<n; i++)
+     {
+        sum = sum + a[i];
+
+     }
+            printf("%d",sum);
+ 
+
+}
+
+//Check odd  or Even  
+#include<stdio.h>
+int main ()
+{
+int n;
+scanf("%d", &n);
+int a[n];
+for(int i=0;i<n;i++)
+{
+        scanf("%d", &a[i]); 
+}
+     odd = 0;
+for(int i=0;i<n;i++)
+{
+     if(a[i] % 2!=0)
+     {
+         odd++
+     }
+}
+        
+return 0;
+}
