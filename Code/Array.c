42d86042415d2c3356a127f47dbf3120679d8c95
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-16 20:59:54 +0600

    Update Array.c

--- Code/Array.c
+++ Code/Array.c
@@ -59,23 +59,41 @@ int main()
 
 //Check odd  or Even  
 #include<stdio.h>
-int main ()
+
+#define N 10
+
+int main()
 {
-int n;
-scanf("%d", &n);
-int a[n];
-for(int i=0;i<n;i++)
-{
-        scanf("%d", &a[i]); 
-}
-     odd = 0;
-for(int i=0;i<n;i++)
-{
-     if(a[i] % 2!=0)
-     {
-         odd++
-     }
-}
-        
-return 0;
+    int a[N], i, even = 0, odd = 0;
+
+    printf("Enter %d integer numbers\n", N);
+    for(i = 0; i < N; i++)
+        scanf("%d", &a[i]);
+
+    printf("\n\nEven numbers in the array are ...\n");
+    for(i = 0; i < N; i++)
+    {
+        if(a[i] % 2 == 0)
+        {
+            printf("%d\n", a[i]);
+            even++;
+        }
+    }
+
+    printf("\nOdd numbers in the array are ...\n");
+    for(i = 0; i < N; i++)
+    {
+        if(a[i] % 2 != 0)
+        {
+            printf("%d\n", a[i]);
+            odd++;
+        }
+    }
+
+    printf("\n\nTotal Even numbers: %d", even);
+    printf("\nTotal Odd numbers: %d\n", odd);
+
+    printf("\n");
+
+    return 0;
 }
