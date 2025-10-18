c903dc2a57df34207c55d31f0ba5dcddff11c90e
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-18 21:28:44 +0600

    Update Array.c

--- Code/Array.c
+++ Code/Array.c
@@ -124,6 +124,34 @@ int main()
 
        }
 
+}
 
+//Removing a value in an array idea
+#include<stdio.h>  
+int main()
+{
+
+     int n; 
+     scanf("%d",&n);  
+     int a[n];
+     for(int i=0; i<n;i++)
+     {
+          scanf("%d",&a[i]);
+
+     }
+
+     int idx;  
+     scanf("%d",&idx); 
+     
+     for(int i=idx; i<n-1;i++)
+    {
+      a[i] = a[i+1];
+    } 
+    n--;
+    for(int i=0; i<n-1;i++)
+    {
+        printf("%d ",a[i]);
+    }
+       return 0;
 
 }
