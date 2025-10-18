256578a7e08402daa21d2396ffdfa35b75ea14c7
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-18 21:40:15 +0600

    Update Array.c

--- Code/Array.c
+++ Code/Array.c
@@ -155,3 +155,19 @@ int main()
        return 0;
 
 }
+
+//Swapping Array 
+#include<stdio.h>
+int main()
+{
+    int a =10; 
+    int b  =20;  
+   int temp = a;
+    a= b; 
+    b = temp;
+
+     printf("A = %d\nB = %d",a,b);
+
+
+}
+
