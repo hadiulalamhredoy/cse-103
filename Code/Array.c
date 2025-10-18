b173f04495aadfbf792087c64eb7468717e271a6
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2025-10-18 21:04:40 +0600

    Update Array.c

--- Code/Array.c
+++ Code/Array.c
@@ -97,3 +97,33 @@ int main()
 
     return 0;
 }
+//Insert a value in an array implementation
+#include<stdio.h> 
+int main()
+{
+     int n; 
+     scanf("%d",&n);  
+     int a[n+1];  
+     for(int i  =0; i<n; i++)
+     {
+        scanf("%d",&a[i]);  
+     }
+       int idx,val; 
+       scanf("%d %d",&idx,&val); 
+      
+      for(int i = n; i >= idx+1; i--)
+      {
+        a[i] = a[i-1];  
+      }
+       a[idx] = val; 
+
+       for(int i =0;i<=n;i++)
+       {
+           printf("%d ",a[i]);
+
+
+       }
+
+
+
+}
